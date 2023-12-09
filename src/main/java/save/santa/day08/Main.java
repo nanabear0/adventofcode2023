1b19f96918839476250a79709db4791630f1dbcd
Denizhan Erdem <denizhanerdem@gmail.com>
2023-12-09 18:06:29 +0000

    day08 apache commons

--- src/main/java/save/santa/day08/Main.java
+++ src/main/java/save/santa/day08/Main.java
@@ -1,5 +1,6 @@
 package save.santa.day08;
 
+import org.apache.commons.math3.util.ArithmeticUtils;
 import org.javatuples.Pair;
 
 import java.io.IOException;
@@ -16,19 +17,6 @@ public class Main {
     public static void main(String[] args) throws IOException {
         part01();
         part02();
-    }
-
-    private static long gcd(long a, long b) {
-        while (b > 0) {
-            long temp = b;
-            b = a % b; // % is remainder
-            a = temp;
-        }
-        return a;
-    }
-
-    private static long lcm(long a, long b) {
-        return a * (b / gcd(a, b));
     }
 
     public static void part02() throws IOException {
@@ -55,7 +43,7 @@ public class Main {
                 steps++;
             } while (!current.endsWith("Z"));
             return (long) steps;
-        }).reduce(Main::lcm).orElseThrow();
+        }).reduce(ArithmeticUtils::lcm).orElseThrow();
 
         System.out.println("part02: " + result);
     }
