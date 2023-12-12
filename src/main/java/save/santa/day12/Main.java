44ddbf33b916a8d62353476aad03ebf1cf00d731
Denizhan Erdem <denizhanerdem@gmail.com>
2023-12-12 22:02:10 +0000

    big boy

--- src/main/java/save/santa/day12/Main.java
+++ src/main/java/save/santa/day12/Main.java
@@ -1,6 +1,7 @@
 package save.santa.day12;
 
 import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.time.DurationUtils;
 import org.apache.commons.lang3.time.StopWatch;
 import org.javatuples.Pair;
 
@@ -8,28 +9,26 @@ import java.io.IOException;
 import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.time.Duration;
 import java.util.*;
-import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
-import java.util.stream.Collectors;
 import java.util.stream.IntStream;
-import java.util.stream.LongStream;
-import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        StopWatch watch = new StopWatch();
-        watch.start();
 //        System.out.println("part01: " + part(1));
-        System.out.println("part02: " + part(3));
-        watch.stop();
-
-        System.out.println("Time: " + watch.formatTime());
+        System.out.println("part02: " + part(5));
     }
+
+    public static StopWatch stopwatch = new StopWatch();
+    public static AtomicInteger completedOnes = new AtomicInteger();
+    public static int totalLines;
 
     public static long part(int copyCount) throws IOException {
         URL resource = Main.class.getClassLoader().getResource("day12-input.txt");
         List<String> lines = Files.lines(Path.of(resource.getFile())).toList();
+        totalLines = lines.size();
 
         List<Pair<String, List<Integer>>> parsedLines = lines.stream().map(line -> {
             var firstSplit = line.split(" ");
@@ -37,28 +36,46 @@ public class Main {
             var s2 = String.join(",", Collections.nCopies(copyCount, firstSplit[1]));
             return Pair.with(s1, Arrays.stream(s2.split(",")).map(Integer::parseInt).toList());
         }).toList();
-        int biggestLine = parsedLines.stream().map(p -> p.getValue0().length()).reduce(Integer::max).orElseThrow();
 
-        return parsedLines.stream().map(pl -> posibilities(pl.getValue0(), pl.getValue1())).reduce(Long::sum).orElse(0L);
+        stopwatch.start();
+        return parsedLines.parallelStream().map(pl -> loggingPossiblities(pl.getValue0(), pl.getValue1())).reduce(Long::sum).orElse(0L);
     }
 
-    static AtomicInteger n = new AtomicInteger(0);
+    public static long loggingPossiblities(String source, List<Integer> target) {
+        long result = posibilities(source, target);
+        int myIndex = completedOnes.incrementAndGet();
+        Duration dur = DurationUtils.toDuration((long) ((double) stopwatch.getTime() / completedOnes.get() * (totalLines - myIndex)), TimeUnit.MILLISECONDS);
+        System.out.printf(
+                "%05d / %05d done after %s. Estimated time left: %02d:%02d:%02d.%04d\n",
+                myIndex,
+                totalLines,
+                stopwatch.formatTime(),
+                dur.toHoursPart(),
+                dur.toMinutesPart(),
+                dur.toSecondsPart(),
+                dur.toMillisPart());
+
+        return result;
+    }
 
     public static long posibilities(String source, List<Integer> target) {
-        System.out.println(n.incrementAndGet());
-        List<Integer> qIndexes = IntStream.range(0, source.length()).filter(i -> source.charAt(i) == '?').boxed().toList();
-        return LongStream.range(0, 1L << qIndexes.size())
-                .parallel()
-                .map(i -> {
-                    Set<Integer> pqi = IntStream.range(0, qIndexes.size()).filter(j -> (1 << j & i) > 0).map(qIndexes::get).boxed().collect(Collectors.toSet());
-                    String stringToTest = StringUtils.strip(IntStream.range(0, source.length())
-                            .map(c -> pqi.contains(c) || source.charAt(c) == '#' ? '#' : '.')
-                            .collect(StringBuilder::new,
-                                    StringBuilder::appendCodePoint,
-                                    StringBuilder::append)
-                            .toString(), ".");
-                    return getLengths(stringToTest).equals(target) ? 1 : 0;
-                }).sum();
+        int firstQ = source.indexOf('?');
+        List<Integer> soFar = getLengths(source.substring(0, firstQ < 0 ? source.length() : firstQ));
+
+        if (firstQ < 0) {
+            return getLengths(source).equals(target) ? 1 : 0;
+        }
+
+        if (soFar.size() > target.size()) return 0;
+
+        if (!IntStream.range(0, soFar.size())
+                .allMatch(i -> i < soFar.size() - 1 ?
+                        Objects.equals(soFar.get(i), target.get(i)) :
+                        soFar.get(i) <= target.get(i))) {
+            return 0;
+        }
+
+        return posibilities(source.replaceFirst("\\?", "#"), target) + posibilities(source.replaceFirst("\\?", "."), target);
     }
 
     public static List<Integer> getLengths(String s) {
