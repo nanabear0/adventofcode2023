58e47a42d36fe31337ad80c25bf71afcb6b30528
Denizhan Erdem <denizhanerdem@gmail.com>
2023-12-13 00:27:27 +0000

    p2 opt

--- src/main/java/save/santa/day12/Main.java
+++ src/main/java/save/santa/day12/Main.java
@@ -30,7 +30,9 @@ public class Main {
 
         List<Pair<String, List<Integer>>> parsedLines = lines.stream()
                 .map(s -> s.split(" ")).map(firstSplit -> Pair.with(
-                        String.join("?", Collections.nCopies(copyCount, firstSplit[0])).replaceAll("\\.+", "."),
+                        StringUtils.strip(
+                                String.join("?", Collections.nCopies(copyCount, firstSplit[0])).replaceAll("\\.+", "."),
+                                "."),
                         Arrays.stream(String.join(",", Collections.nCopies(copyCount, firstSplit[1])).split(","))
                                 .map(Integer::parseInt).toList()))
                 .toList();
@@ -45,46 +47,27 @@ public class Main {
         if (possibilityCache.containsKey(key)) return possibilityCache.get(key);
 
         int firstQ = source.indexOf('?');
-        List<Integer> soFar = getLengths(source.substring(0, firstQ < 0 ? source.length() : firstQ));
-
-        if (firstQ < 0) {
-            long val = getLengths(source).equals(target) ? 1 : 0;
-            possibilityCache.put(key, val);
-            return val;
-        }
-
-        if (soFar.size() > target.size()) {
-            possibilityCache.put(key, 0L);
-            return 0L;
-        }
-
-        if (!IntStream.range(0, soFar.size())
-                .allMatch(i -> i < soFar.size() - 1 ?
-                        Objects.equals(soFar.get(i), target.get(i)) :
-                        soFar.get(i) <= target.get(i))) {
-            possibilityCache.put(key, 0L);
-            return 0;
-        }
+        int firstD = source.indexOf('.');
+        int firstH = source.indexOf('#');
 
         long val;
-        if (soFar.size() > 1) {
-            String newSource = source.substring(StringUtils.ordinalIndexOf("." + source, ".#", soFar.size()));
-            List<Integer> newTarget = target.stream().skip(soFar.size() - 1).toList();
-            val = posibilities(newSource.replaceFirst("\\?", "#"), newTarget) +
-                    posibilities(newSource.replaceFirst("\\?", "."), newTarget);
+        if (target.isEmpty() && firstH >= 0) {
+            val = 0;
+        } else if (firstH < 0 && firstQ < 0) {
+            val = target.isEmpty() ? 1 : 0;
+        } else if ((firstD < firstQ || firstQ < 0) && firstD > firstH) {
+            val = target.get(0) == firstD ? posibilities(
+                    StringUtils.strip(source.substring(firstD), "."),
+                    target.stream().skip(1).toList()
+            ) : 0;
+        } else if (firstQ >= 0) {
+            val = posibilities(StringUtils.strip(source.replaceFirst("\\?", "#"), "."), target) +
+                    posibilities(StringUtils.strip(source.replaceFirst("\\?", "."), "."), target);
         } else {
-            val = posibilities(source.replaceFirst("\\?", "#"), target) +
-                    posibilities(source.replaceFirst("\\?", "."), target);
+            val = target.size() == 1 && target.get(0) == source.length() ? 1 : 0;
         }
 
         possibilityCache.put(key, val);
         return val;
-    }
-
-    public static List<Integer> getLengths(String s) {
-        return Arrays.stream(s.split("\\.+"))
-                .filter(StringUtils::isNoneEmpty)
-                .map(String::length)
-                .toList();
     }
 }
