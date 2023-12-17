ec1b8605f0d47fc0906247f4f364ade5afb1595a
Denizhan Erdem <denizhanerdem@gmail.com>
2023-12-17 17:12:17 +0000

    p2

--- src/main/java/save/santa/day16/Main.java
+++ src/main/java/save/santa/day16/Main.java
@@ -22,41 +22,51 @@ public class Main {
             }
         }
         Pair<Integer, Integer> target = Pair.with(lines.get(0).length() - 1, lines.size() - 1);
+//        System.out.println("part01: " + doPart(target, lossMap, 0, 3));
+        System.out.println("part02: " + doPart(target, lossMap, 4, 10));
+    }
+
+    private static long doPart(Pair<Integer, Integer> target, HashMap<Pair<Integer, Integer>, Integer> lossMap, int minTurn, int maxStraight) {
         Map<Quartet<Integer, Integer, Integer, Integer>, Integer> distance = new HashMap<>();
+        Set<Quartet<Integer, Integer, Integer, Integer>> finished = new HashSet<>();
+        PriorityQueue<Quartet<Integer, Integer, Integer, Integer>> frontier = new PriorityQueue<>(Comparator.comparing(distance::get));
         distance.put(Quartet.with(0, 0, 0, 0), 0);
-        Set<Quartet<Integer, Integer, Integer, Integer>> finishedProcessing = new HashSet<>();
+        frontier.add(Quartet.with(0, 0, 0, 0));
         int result;
 
         while (true) {
-            Quartet<Integer, Integer, Integer, Integer> current = distance.entrySet().stream()
-                    .filter(x -> !finishedProcessing.contains(x.getKey()))
-                    .filter(x -> distance.containsKey(x.getKey()))
-                    .min(Map.Entry.comparingByValue())
-                    .orElseThrow().getKey();
+            Quartet<Integer, Integer, Integer, Integer> current = frontier.remove();
             if (Objects.equals(current.getValue0(), target.getValue0()) && current.getValue1().equals(target.getValue1())) {
                 result = distance.get(current);
                 break;
             }
 
-            List<Quartet<Integer, Integer, Integer, Integer>> nps =
+            List<Pair<Quartet<Integer, Integer, Integer, Integer>, Integer>> nps =
                     Stream.of(Pair.with(0, 1), Pair.with(0, -1), Pair.with(1, 0), Pair.with(-1, 0))
-                            .map(dir -> move(current, dir))
+                            .map(dir -> move(current, dir, minTurn, maxStraight))
                             .filter(Objects::nonNull)
                             .filter(x -> lossMap.containsKey(Pair.with(x.getValue0(), x.getValue1())))
-                            .filter(x -> !finishedProcessing.contains(x))
+                            .filter(x -> !finished.contains(x))
+                            .map(np -> Pair.with(np, distance.get(current) + lossMap.get(Pair.with(np.getValue0(), np.getValue1()))))
                             .toList();
-            nps.forEach(np -> distance.merge(np, distance.get(current) + lossMap.get(Pair.with(np.getValue0(), np.getValue1())), Math::min));
-            finishedProcessing.add(current);
+            var nfa = nps.stream().filter(np -> distance.getOrDefault(np.getValue0(), Integer.MAX_VALUE) > np.getValue1()).map(Pair::getValue0).toList();
+            nps.forEach(np -> distance.merge(np.getValue0(), np.getValue1(), Math::min));
+            finished.add(current);
+            frontier.addAll(nfa);
         }
 
-        System.out.println("part01: " + result);
+        return result;
     }
 
-    public static Quartet<Integer, Integer, Integer, Integer> move(Quartet<Integer, Integer, Integer, Integer> initial, Pair<Integer, Integer> dir) {
-        if (dir.getValue0() != 0 && initial.getValue2() / dir.getValue0() < 0) return null;
-        if (dir.getValue1() != 0 && initial.getValue3() / dir.getValue1() < 0) return null;
-        if (dir.getValue0() != 0 && initial.getValue2() + dir.getValue0() > 3) return null;
-        if (dir.getValue1() != 0 && initial.getValue3() + dir.getValue1() > 3) return null;
+    public static Quartet<Integer, Integer, Integer, Integer> move(Quartet<Integer, Integer, Integer, Integer> initial, Pair<Integer, Integer> dir, int minTurn, int maxStraight) {
+        if (!(initial.getValue2() == 0 && initial.getValue3() == 0)) {
+            if (dir.getValue0() != 0 && initial.getValue2() * dir.getValue0() < 0) return null;
+            if (dir.getValue1() != 0 && initial.getValue3() * dir.getValue1() < 0) return null;
+            boolean isTurn = (dir.getValue0() != 0 ^ initial.getValue2() != 0) || (dir.getValue1() != 0 ^ initial.getValue3() != 0);
+            if (isTurn && Math.abs(initial.getValue2() + initial.getValue3()) < minTurn) return null;
+            if (!isTurn && Math.abs(initial.getValue2() + initial.getValue3()) >= maxStraight) return null;
+        }
+
         return Quartet.with(
                 initial.getValue0() + dir.getValue0(),
                 initial.getValue1() + dir.getValue1(),
